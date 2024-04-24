f3a96193a293e3388a253d8853808a24338959c2
Manuel Santos (CTW) <manuel.rs.santos@ctw.bmwgroup.com>
2024-04-24 10:32:24 +0100

    Added libs

--- functions/index.ts
+++ functions/index.ts
@@ -1,6 +1,7 @@
 import {PagesFunction} from "@cloudflare/workers-types";
+import {messages} from "@utils/constants";
 
 export const onRequest: PagesFunction = async (context) => {
 
-    return new Response("Hello!");
+    return new Response(messages.hello);
 }
