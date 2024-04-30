f6d7554f1a1990cf621b3a697e160ffea426688c
Manuel Fé Santos <132622567+manuelfesantos@users.noreply.github.com>
2024-04-30 02:19:07 +0100

    Edit code

--- functions/carts/[id]/index.ts
+++ functions/carts/[id]/index.ts
@@ -8,15 +8,5 @@ interface Env {
 }
 
 export const onRequest: PagesFunction<Env> = async (context) => {
-  setTimer();
-
-  context.waitUntil(
-    fetch(
-      "https://api.cloudflare.com/client/v4/pages/webhooks/deploy_hooks/8ae4a7da-a70e-4c75-a114-eb1e82fa0aa2",
-      {
-        method: "POST",
-      },
-    ),
-  );
   return Response.json({ message: "build successful!" });
 };
