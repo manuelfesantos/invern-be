496f23cafaf26ce965b43791cb9722b10a8f3926
Manuel Fé Santos <manuelfesantos@gmail.com>
2025-03-23 02:31:47 +0000

    SPIRIT-0: cache country in middleware call

---
+++ libs/utils/cache/country-cache.ts
@@ -0,0 +1,16 @@
+import { Country } from "@country-entity";
+
+const countriesMap = new Map<string, Country>();
+
+const addCountry = (country: Country): void => {
+  countriesMap.set(country.code, country);
+};
+
+const getCountryByCode = (code: string): Country | undefined => {
+  return countriesMap.get(code);
+};
+
+export const countryCache = {
+  add: addCountry,
+  get: getCountryByCode,
+};
