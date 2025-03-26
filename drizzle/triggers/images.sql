44ace6c349f0da5759343fe87fe224189020a716
Manuel Fé Santos <manuelfesantos@gmail.com>
2025-03-26 02:53:55 +0000

    SPIRIT-0: fix add-test-data script

--- drizzle/triggers/images.sql
+++ drizzle/triggers/images.sql
@@ -2,5 +2,5 @@ CREATE TRIGGER IF NOT EXISTS update_lastModifiedAt_images
     BEFORE UPDATE ON images
     FOR EACH ROW
 BEGIN
-    UPDATE images SET lastModifiedAt = strftime('%Y-%m-%dT%H:%M:%f', 'now') WHERE id = NEW.id;
+    UPDATE images SET lastModifiedAt = strftime('%Y-%m-%dT%H:%M:%f', 'now') WHERE url = NEW.url;
 END;
