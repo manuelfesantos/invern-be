c3e3f1f09369b7277f6cd63fe6b481614d5e9e0c
Manuel Fé Santos <manuelfesantos@gmail.com>
2025-04-10 03:06:07 +0100

    SPIRIT-0: fix validate user fetch by email

--- libs/modules/user/use-cases/signup/validate-user.ts
+++ libs/modules/user/use-cases/signup/validate-user.ts
@@ -1,5 +1,10 @@
 import { selectUserByEmail, updateUser } from "@user-db";
-import { toUserDTO, UserDTO, ValidateEmailSecretBody } from "@user-entity";
+import {
+  toUserDTO,
+  UserDTO,
+  UserValidationStatusEnum,
+  ValidateEmailSecretBody,
+} from "@user-entity";
 import { errors } from "@error-handling-utils";
 import { getLoggedInRefreshToken, getLoggedInToken } from "@jwt-utils";
 import { getAuthSecret, setAuthSecret } from "@kv-adapter";
@@ -20,7 +25,7 @@ export const validateUser = async (
   email: string,
   secret: ValidateEmailSecretBody,
 ): Promise<ReturnType> => {
-  const user = await selectUserByEmail(email);
+  const user = await selectUserByEmail(email, UserValidationStatusEnum.ALL);
   if (!user) throw errors.USER_NOT_FOUND();
 
   if (!user.isValidated) {
