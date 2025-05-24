8a77cfa990c2f34f7f7ff02d15195ebc8c902265
Manuel Fé Santos <manuelfesantos@gmail.com>
2025-05-24 04:40:18 +0100

    SPIRIT-0: add log for payment insert retries

--- libs/modules/order/use-cases/payment/utils/retry-payment.ts
+++ libs/modules/order/use-cases/payment/utils/retry-payment.ts
@@ -1,3 +1,6 @@
+import { logger } from "@logger-utils";
+import { LoggerUseCaseEnum } from "@logger-entity";
+
 const MAX_RETRIES = 3;
 const ONE_INCREMENT = 1;
 const MIN_RETRIES = 0;
@@ -9,6 +12,13 @@ export const withRetry = async <T, K>(
   try {
     return await callBack(args);
   } catch (error) {
+    logger().warn("Retrying payment operation", {
+      useCase: LoggerUseCaseEnum.RETRY_INSERT_PAYMENT,
+      data: {
+        error: error instanceof Error ? error.message : error,
+        retries,
+      },
+    });
     if (retries < MAX_RETRIES) {
       return withRetry(args, callBack, retries + ONE_INCREMENT);
     }
