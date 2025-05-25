4e356ee54437a31cdc428016adac880ae5478a33
Manuel Fé Santos <manuelfesantos@gmail.com>
2025-05-25 18:40:20 +0100

    SPIRIT-0: fix cart insert query action

--- libs/db/order/actions/delete.ts
+++ libs/db/order/actions/delete.ts
@@ -2,8 +2,17 @@ import { db } from "@db";
 import { ordersTable } from "@schema";
 import { eq } from "drizzle-orm";
 import { actionBuilder } from "@generics-db";
+import { logger } from "@logger-utils";
+import { LoggerUseCaseEnum } from "@logger-entity";
 
-const deleteOrderQuery = (orderId: string) =>
-  db().delete(ordersTable).where(eq(ordersTable.id, orderId));
+const deleteOrderQuery = (orderId: string) => {
+  logger().info("deleting order", {
+    useCase: LoggerUseCaseEnum.DELETE_ORDER,
+    data: {
+      orderId,
+    },
+  });
+  return db().delete(ordersTable).where(eq(ordersTable.id, orderId));
+};
 
 export const getDeleteOrderAction = actionBuilder(deleteOrderQuery);
